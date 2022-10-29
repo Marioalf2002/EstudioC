ca3ad7b309094a6ff1120e721772c776e26f6c1c
Mario Hernandez <marioalf200216@gmail.com>
2022-10-28 22:17:28 -0500

    Se añaden mas procesos en el projecto de Matrices

--- Arreglos_Matrices/main.cpp
+++ Arreglos_Matrices/main.cpp
@@ -9,10 +9,10 @@ using namespace std;
 int main()
 {
 	int M[3] = { 1, 2, 3 };														//arreglo de 3 espacios.
-	int j[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };							    //cantidad filas y columnas una matriz de 3*3 Filas = {}{}{} Columnas = 1,2,3  
+	int N[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };							    //cantidad filas y columnas una matriz de 3*3 Filas = {}{}{} Columnas = 1,2,3  
 	int k[4][2] = { {1,2}, {3,4}, {5,6}, {7,8} };
 
-	cout << "Elemento de la matriz: " << j[0][1] << endl;
+	cout << "Elemento de la matriz es: " << N[0][1] << endl;
 
 	for (int i = 0; i < 4; i++) {												//Imprimir toda una matriz i=filas j=columna
 		cout << "Elemento de la matriz ";
@@ -22,6 +22,8 @@ int main()
 		cout << endl;
 	}
 
+	//AGREGAR VALORES A LA MATRIZ
+	cout << "\nAGREGAR VALORES A AL MATRIZ\n";
 	int fila = 0, columna = 0;
 	cout << "Ingresar cantidad de Filas: ";
 	cin >> fila;
@@ -32,37 +34,49 @@ int main()
 
 	for (int i = 0; i < fila; i++) {												//Imprimir toda una matriz i=filas j=columna
 		for (int j = 0; j < columna; j++) {
-			cout << "Porfavor ingrese elemento en la fila " << i << " y Columna " << j << ": " << endl;
+			cout << "Porfavor ingrese elemento en la fila " << i << " y Columna " << j << ": ";
 			cin >> L[fila][columna];
 		}
 	}
+	cout << "\nMATRIZ\n";
+
+	for (i = 0; i < fila; i++) {
+		for (j = 0; j < columna; j++) {
+			cout << matriz[i][j] << " ";
+		}
+		cout << endl;
+	}
 
 	//SUMA DE MATRICES POR POSICION EN OTRA MATRIZ
-
-	int array_1[8];
-	int array_2[8];
-	int array_3[8];
+	cout << "\nSUMA DE MATRICES POR POSICION EN OTRA MATRIZ\n";
+	int vector_1[8];
+	int vector_2[8];
+	int vector_3[8];
 	int f, vec1, vec2;
+
 	cout << "Ingresar cantidad de numeros que desea en el vector 1: ";
 	cin >> vec1;
+
 	for (f = 0; f < vec1; f += 1) {
 		cout << "valor posicion " << f << ": ";
-		cin >> array_1[f];
+		cin >> vector_1[f];
 	}
 	cout << endl;
+
 	cout << "Ingresar cantidad de numeros que desea en el vector 2: ";
 	cin >> vec2;
 	for (f = 0; f < vec2; f += 1) {
 		cout << "valor posicion " << f << ": ";
-		cin >> array_2[f];
+		cin >> vector_2[f];
 	}
 	if (vec1 == vec2)
 	{
+		cout << "\n";
 		for (f = 0; f < vec1; f += 1) {
-			array_3[f] = array_1[f] + array_2[f];
+			vector_3[f] = vector_1[f] + vector_2[f];
 		}
 		for (f = 0; f < vec1; f += 1) {
-			cout << array_1[f] << " " << array_2[f] << " " << array_3[f] << endl;
+			cout << vector_1[f] << " " << vector_2[f] << " " << vector_3[f] << endl;
 		}
 	}
 	else {
@@ -70,5 +84,56 @@ int main()
 		cout << "NO SE PUEDEN SUMAR PORQUE LOS VECTORES NO SON DE IGUAL TAMAÑO" << endl;
 	}
 
+	//IMPRIMIR DIAGONALES DE UNA MATRIZ
+	cout << "\nIMRIMIR DIAGONALE\n";
+	int a, b, fi, c, matriz[100][100];
+
+	cout << "Ingresa el numero de filas de la matriz:";
+	cin >> fi;
+
+	cout << "Ingresa el numero de columnas de la matriz : ";
+	cin >> c;
+
+	if (fi > 0 && c > 0) {
+		if (fi == c) {
+			for (a = 0; a < fi; a++) {
+				for (b = 0; b < c; b++) {
+					cout << "Ingresa un numero en posicion "<<a<<" "<<b<<": ";
+					cin >> matriz[a][b];
+				}
+			}
+
+			cout << "\nMATRIZ\n";
+
+			for (a = 0; a < fi; a++) {
+				for (b = 0; b < c; b++) {
+					cout << matriz[a][b] << " ";
+				}
+				cout << endl;
+			}
+
+			cout << "DIAGONAL PRINCIPAL:\n";
+
+			for (a = 0; a < fi; a++) {
+				cout << matriz[a][a] << " ";
+				b--;
+			}
+
+			cout << endl;
+
+			b = c - 1;
+
+			cout << "DIAGONAL SEGUNDARIA:\n";
+
+			for (a = 0; a < fi; a++) {
+				cout << matriz[a][b] <<" ";
+				b--;
+			}
+		}
+		else
+		{
+			cout << "NO SE PUEDE HACER LA OPERCION PORQUE SON DIFERENTES.\n";
+		}
+	}
 	return 0;
 }
